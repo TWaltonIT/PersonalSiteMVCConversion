992624314c5ab43bc2af587f432e5669aa460435
TWaltonIT <tww1998@yahoo.com>
2024-03-19 15:13:34 -0500

    Converted Resume

--- PersonalSiteMVCConversion/Controllers/HomeController.cs
+++ PersonalSiteMVCConversion/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace PersonalSiteMVCConversion.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Resume()
+        {
+            return View();
+        }
     }
 }
